06d8c843d19d983e435ffea1bd1ddcc2a8721fdf
vikas1369 <do.vikas1369@gmail.com>
2018-12-11 12:58:28 +0530

    upload image with rotation

--- Minio/upload_images.py
+++ Minio/upload_images.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+from scipy import ndimage
+from PIL import Image
 import datetime, time, os
 from picamera import PiCamera
 from time import sleep
@@ -7,7 +9,7 @@ from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
                          BucketAlreadyExists)
 #Initialize camera module
 camera=PiCamera()
-
+start_time = time.time()
 # Initialize minioClient with an endpoint and access/secret keys.
 minioClient = Minio('172.18.22.9:9000',
                     access_key='AKIAIOSFODNN734dfhdfh',
@@ -16,13 +18,20 @@ minioClient = Minio('172.18.22.9:9000',
 
 
 
-camera.rotation = -90 #Rotate camera image by 90 degree anti-clkwise
+#camera.rotation = -90 #Rotate camera image by 90 degree anti-clkwise
 prefix="00000"
 try:
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
-    camera.capture('/home/pi/cron'+st+'.jpeg', format = 'jpeg', resize = None)
-    minioClient.fput_object('iotlab3', (st+'.jpeg'), '/home/pi/cron'+st+".jpeg")
-    os.remove(('/home/pi/cron'+st+'.jpeg')) #Deleting the file after it has been uploaded to the object
+    original_filepath = '/home/pi/cron'+st+'.jpeg'
+    rotated_filepath = '/home/pi/cron/rotated_image.jpeg'
+    camera.capture(original_filepath)
+    im1 = Image.open(original_filepath,mode='r')
+    im1 = ndimage.rotate(im1, 90)
+    im = Image.fromarray(im1,mode='RGB')
+    im.save(rotated_filepath,quality=100)
+    minioClient.fput_object('iotlab3', (st+'.jpeg'), rotated_filepath)
+    os.remove(rotated_filepath)
+    os.remove(original_filepath) #Deleting the file after it has been uploaded to the object
 finally:
     camera.close() #Free up camera resources
