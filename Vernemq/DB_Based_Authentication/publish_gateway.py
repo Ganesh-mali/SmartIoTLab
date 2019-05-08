7744762796762c2361f56da30f336354fcd9d387
Vika Yadav <do.vikas1369@gmail.com>
2019-05-08 11:58:44 +0530

    ESP32 display programs added

--- Vernemq/DB_Based_Authentication/publish_gateway.py
+++ Vernemq/DB_Based_Authentication/publish_gateway.py
@@ -4,15 +4,14 @@ import time
 def on_publish(client, userdata, mid):
     print("mid: "+str(mid))
  
-client = paho.Client(client_id="test-client1",clean_session=True)
+client = paho.Client(client_id="gateway_lab3",clean_session=True)
 client.on_publish = on_publish
-client.username_pw_set("test-user1","123")
+client.username_pw_set("lab3gateway","g0tew0y10b3")
 client.connect("172.18.22.9", 1886)
 client.loop_start()
  
 while True:
     temperature = 10
-    (rc, mid) = client.publish("c/b/vikas", str(temperature), qos=1)
+    (rc, mid) = client.publish("vjti/csriot/glab3/temp", str(temperature), qos=1)
     time.sleep(5)
     #print(client.getTopic())
-
